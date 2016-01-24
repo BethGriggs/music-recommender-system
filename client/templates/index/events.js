24ca82394672986088a0b5aee01b07f9ac74d620
Dylan Scott <dylanrhysscott@gmail.com>
2016-01-24 22:29:51 +0000

    Room Join with querystring mood and genre

--- client/templates/index/events.js
+++ client/templates/index/events.js
@@ -1,3 +1,13 @@
 Template.index.rendered = function() {
   $('select').select2();
 }
+
+Template.index.events({
+  "submit #joinRoom": function(e) {
+    e.preventDefault();
+    var controls = e.target.elements;
+    var url = 'http://' + window.location.hostname + ':' + window.location.port + '/room/' + controls[2].value + '?mood=' + controls[0].value + '&?genre=' + controls[1].value;
+    window.location.href = url;
+    return false;
+  }
+});
