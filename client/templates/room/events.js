6f288c3b89c76371ebb85e09c2a9296a72860d97
Dylan Scott <dylanrhysscott@gmail.com>
2016-01-25 17:55:47 +0000

    Added code for algorithm base

---
+++ client/templates/room/events.js
@@ -0,0 +1,6 @@
+Template.room.rendered = function() {
+  // Run this function on template load - Beth add your algorithm stuff here like AJAX Musicovery
+  // Use HTTP.call - http://docs.meteor.com/#/full/http for AJAX
+  var data = this.data; // Data available to template from router - Room Data
+  console.log(data);
+}
