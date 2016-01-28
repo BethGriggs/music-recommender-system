d2ed9a4caab7e0f01529c951c474c553c97675f5
Dylan Scott <dylanrhysscott@gmail.com>
2016-01-28 16:52:29 +0000

    Meteor AJAX

--- client/templates/playRoom/events.js
+++ client/templates/playRoom/events.js
@@ -3,20 +3,35 @@ Template.playRoom.rendered = function() {
     // Run this function on template load - Beth add your algorithm stuff here like AJAX Musicovery
     // Use HTTP.call - http://docs.meteor.com/#/full/http for AJAX
     var data = this.data; // Data available to template from router - Room Data
-    console.log(data);
+    var moodValues;
+    HTTP.get('/moodValues.json', function(err, result) {
+      if(err) {
 
-    var roomMoods = [];
-    for (var user in data.users){
-        roomMoods.push(data.users[user].mood);
-    }
-    console.log("Room Moods 0: " + roomMoods.length);
-    var total= 0;
+      }
 
-    $.get( "/moodValues.json", function( moodValues ) {
-        for (var i=0; i< roomMoods.length; i++){
-                    total+= moodValues[roomMoods[i]][0].index;
-        }
-
-        console.log("average index" + total/ roomMoods.length);
+      if(result) {
+        moodValues = JSON.parse(result.content);
+      }
     });
+
+    // for (var user in data.users){
+    //     var mood = data.users[user].mood;
+    //     console.log(user);
+    //     console.log(mood);
+    // }
+
+    // var roomMoods = [];
+    // for (var user in data.users){
+    //     roomMoods.push(data.users[user].mood);
+    // }
+    // console.log("Room Moods 0: " + roomMoods.length);
+    // var total= 0;
+    //
+    // $.get( "/moodValues.json", function( moodValues ) {
+    //     for (var i=0; i< roomMoods.length; i++){
+    //                 total+= moodValues[roomMoods[i]][0].index;
+    //     }
+    //
+    //     console.log("average index" + total/ roomMoods.length);
+    // });
 }
